5952b314b88eb3a4b2c1f4932581dc95d5aeb0eb
Preet Patel <preet.patel2@ontariotechu.net>
2022-11-30 22:41:16 -0500

    Configuring lights.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('snoop/', views.index, name='snoop'),
     path('home/', views.home, name='home'),
     path('lights/', views.lights, name='light')
 ]
