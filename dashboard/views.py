79b1516f0a987b78882c60476125cebb3d300b3f
Preet Patel <preet.patel2@ontariotechu.net>
2022-12-01 08:20:28 -0500

    Tes

--- dashboard/views.py
+++ dashboard/views.py
@@ -24,34 +24,31 @@ class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
 
 def home(request):
-    # print(dir(request))
-    # print(request.POST)
-    # print(request.body)
-    # if 'on' in request.POST:
-    #     values = {"name": "on"}
-    #     r=request.put('http://127.0.0.1:8000/state/1/', data=values, auth=('username', 'password'))
-    #     result=r.text
-    #     output = json.loads(result)
-    #     out=output['name']
-    # if 'off' in request.POST:
-    #     values = {"name": "off"}
-    #     r=request.Request.put('http://127.0.0.1:8000/state/1/', data=values, auth=('username', 'password'))
-    #     result=r.text
-    #     output = json.loads(result)
-    #     out=output['name']
-    # if 'auto' in request.POST:
-    #     values = {"name": "auto"}
-    #     r=request.Request.put('http://127.0.0.1:8000/mode/1/', data=values, auth=('username', 'password'))
-    #     result=r.text
-    #     output = json.loads(result)
-    #     out=output['name']
-    # if 'manual' in request.POST:
-    #     values = {"name": "manual"}
-    #     r=request.Request.put('http://127.0.0.1:8000/mode/1/', data=values, auth=('username', 'password'))
-    #     result=r.text
-    #     output = json.loads(result)
-    #     out=output['name']
-    
+    out=""
+    if 'on' in request.POST:
+        values = {"name": "on"}
+        r=request.put('http://127.0.0.1:8000/state/1/', data=values, auth=('username', 'password'))
+        result=r.text
+        output = json.loads(result)
+        out=output['name']
+    if 'off' in request.POST:
+        values = {"name": "off"}
+        r=request.Request.put('http://127.0.0.1:8000/state/1/', data=values, auth=('username', 'password'))
+        result=r.text
+        output = json.loads(result)
+        out=output['name']
+    if 'auto' in request.POST:
+        values = {"name": "auto"}
+        r=request.Request.put('http://127.0.0.1:8000/mode/1/', data=values, auth=('username', 'password'))
+        result=r.text
+        output = json.loads(result)
+        out=output['name']
+    if 'manual' in request.POST:
+        values = {"name": "manual"}
+        r=request.Request.put('http://127.0.0.1:8000/mode/1/', data=values, auth=('username', 'password'))
+        result=r.text
+        output = json.loads(result)
+        out=output['name']
     r=request.Request.get('http://127.0.0.1:8000/mode/1/', auth=('username', 'password'))
     result=r.text
     output = json.loads(result)
@@ -60,5 +57,4 @@ def home(request):
     result=r.text
     output = json.loads(result)
     currentstate=output['name']
-
     return render('lights.html',{'r':out, 'currentmode':currentmode, 'currentstate':currentstate}, context_instance=RequestContext(request))
