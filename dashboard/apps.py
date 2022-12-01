895769335789a0c58f90346d8519acaa1ea9a563
preet <preet.patel2@ontariotechu.net>
2022-12-01 13:39:42 -0500

    Test

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -12,15 +12,15 @@ def on_connect(mqtt_client, userdata, flags, rc):
 def on_message(mqtt_client, userdata, msg):
    print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
-client.connect(
-    host=settings.MQTT_SERVER,
-    port=settings.MQTT_PORT,
-    keepalive=settings.MQTT_KEEPALIVE
-)
+# client = mqtt.Client()
+# client.on_connect = on_connect
+# client.on_message = on_message
+# client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
+# client.connect(
+#     host=settings.MQTT_SERVER,
+#     port=settings.MQTT_PORT,
+#     keepalive=settings.MQTT_KEEPALIVE
+# )
 
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
