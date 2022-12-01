d0da6cadc55336f4f96a7bfb0ac3d6064230a1a0
preet <preet.patel2@ontariotechu.net>
2022-12-01 13:17:37 -0500

    Test

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,27 +10,6 @@ from mqtt_test.mqtt import client as mqtt_client
 # Create your views here.
 from django.http import HttpResponse
 
-
-def on_connect(mqtt_client, userdata, flags, rc):
-   if rc == 0:
-       print('Connected successfully')
-       mqtt_client.subscribe('django/mqtt')
-   else:
-       print('Bad connection. Code:', rc)
-
-def on_message(mqtt_client, userdata, msg):
-   print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
-client.connect(
-    host=settings.MQTT_SERVER,
-    port=settings.MQTT_PORT,
-    keepalive=settings.MQTT_KEEPALIVE
-)
-
 def index(request):
     return render(request,"index.html")
 
