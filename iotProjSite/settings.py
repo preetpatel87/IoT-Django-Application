2a33e7f68cf1f492db268819d73875ec2ad903df
preet <preet.patel2@ontariotechu.net>
2022-12-01 13:08:26 -0500

    Test MQTT

--- iotProjSite/settings.py
+++ iotProjSite/settings.py
@@ -138,28 +138,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# def on_connect(mqtt_client, userdata, flags, rc):
-#    if rc == 0:
-#        print('Connected successfully')
-#        mqtt_client.subscribe('django/mqtt')
-#    else:
-#        print('Bad connection. Code:', rc)
-
-# def on_message(mqtt_client, userdata, msg):
-#    print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
-
 MQTT_SERVER = '9511cb223d8c4115921ca0e7542cc769.s2.eu.hivemq.cloud'
 MQTT_PORT = 8883
 MQTT_KEEPALIVE = 60
 MQTT_USER = 'username'
 MQTT_PASSWORD = 'password'
 
-client = mqtt.Client()
-# client.on_connect = on_connect
-# client.on_message = on_message
-client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
-client.connect(
-    host=settings.MQTT_SERVER,
-    port=settings.MQTT_PORT,
-    keepalive=settings.MQTT_KEEPALIVE
-)
