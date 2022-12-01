bb4f5ce6b6df5bda25408aa74361eb1eb8b2256a
Preet Patel <preet.patel2@ontariotechu.net>
2022-11-30 23:52:38 -0500

    Test

--- iotProjSite/settings.py
+++ iotProjSite/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=str(os.environ.get('DEBUG'))=='1'
 
-ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
-ALLOWED_HOSTS = []
-if ENV_ALLOWED_HOST is not None:
-    ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 
